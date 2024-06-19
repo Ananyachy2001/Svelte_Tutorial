c90ed7bc435ae42d9dc8b2c651a360b8dc8afb0e
Ananyachy2001 <ananyachy2001@gmail.com>
2024-06-19 16:06:44 +0600

    forms

--- src/App.svelte
+++ src/App.svelte
@@ -1,80 +1,56 @@
 <script>
   import Modal from './Modal.svelte';
-let showModal = false;
-  let people = [
-    { name: "Joy", beltColour: "black", age: 26, id: 1 },
-    { name: "Floran", beltColour: "blue", age: 46, id: 2 },
-    { name: "Jimmy", beltColour: "red", age: 35, id: 3 },
-  ];
+  import AddPersonForm from './AddPersonForm.svelte';
 
-  const handleClick = (id) => {
-    //delete the person from people
-    people = people.filter((person) => person.id != id); //reassign
+  let showModal = false;
+
+  let toggleModal = () => {
+    showModal = !showModal;
   };
 
-  const toggleModal = () =>{
-    showModal = !showModal;
-  }
+	let people = [
+    { name: 'yoshi', beltColour: 'black', age: 25, id: 1 },
+    { name: 'mario', beltColour: 'orange', age: 45, id: 2 },
+    { name: 'luigi', beltColour: 'brown', age: 35, id: 3 }
+  ];
 
-let num = 7;
+  const handleClick = (e, id) => {
+    people = people.filter(person => person.id != id);
+    console.log(e);
+  };
 </script>
-<!-- <Modal message="Hey, i'm a prop value" isPromo={true} /> -->
-<Modal  showModal={showModal} on:click={toggleModal}>
-  <!-- <h3>Add a new person</h3> -->
-  <form action="">
-    <input type="text" placeholder="name">
-    <input type="text" placeholder="belt colour">
-    <button>Add Person</button>
-  </form>
-  <div slot="title">
-  <h3>Add a new person</h3>
 
-  </div>
+<Modal {showModal} on:click={toggleModal}>
+  <AddPersonForm />
 </Modal>
-{#if num > 20}
-  <p>Greater than 20</p>
-{:else if num > 5}
-  <p>Greater than 5</p>
-{:else}
-  <p>Not greater than 5</p>
-{/if}
+
 <main>
-  <div>
-    <button on:click|once={toggleModal}>Open Modal</button>
-    
-    {#each people as person (person.id)}
-      <div>
-        <h4>{person.name}</h4>
-        {#if person.beltColour === "black"}
-          <p><strong>MASTER NINJA</strong></p>
-        {/if}
-        <p>{person.age} years old, {person.beltColour} belt.</p>
-        <button on:click={() => handleClick(person.id)}>delete</button>
-      </div>
-    {:else}
-      <p>There are no people to show</p>
-    {/each}
-  </div>
+  <button on:click={toggleModal}>Open Modal</button>
+  {#each people as person (person.id)}
+    <div>
+      <h4>{person.name}</h4>
+      {#if person.beltColour === 'black'}
+        <p><strong>MASTER NINJA</strong></p>
+      {/if}
+      <p>{person.age} years old, {person.beltColour} belt.</p>
+      <button on:click={(e) => handleClick(e, person.id)}>delete</button>
+    </div>
+  {:else}
+    <p>There are no people to show...</p>
+  {/each}
 </main>
 
 <style>
-  main {
-    text-align: center;
-    padding: 1em;
-    max-width: 240px;
-    margin: 0 auto;
-  }
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
 
-  h1 {
-    color: #ff3e00;
-    text-transform: uppercase;
-    font-size: 4em;
-    font-weight: 100;
-  }
-
-  @media (min-width: 640px) {
-    main {
-      max-width: none;
-    }
-  }
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
 </style>
--- src/Modal.svelte
+++ src/Modal.svelte
@@ -1,41 +1,33 @@
 <script>
-    //one - makes sure the event can only fire once(removes handler)
-    //preventDefault - prevent the default action (run else.preventDefault())
-    //self - only fires the event if the clicked element is the target
-
-export let showModal = false;
-export let isPromo = false;
+  export let showModal = false;
+  export let isPromo = false;
 </script>
 
 {#if showModal}
-<div class="backdrop" class:promo={isPromo} on:click self>
+  <div class="backdrop" class:promo={isPromo} on:click|self>
     <div class="modal">
-        <slot></slot>
-        <slot name="title"></slot>
+      <slot></slot>
     </div>
-</div>
+  </div>
 {/if}
 
 <style>
-
-    .backdrop {
-        width: 100%;
-        height: 100%;
-        position: fixed;
-        background: rgba(0,0,0,0.8);
-    }
-
-    .modal{
-        padding: 10px;
-        border-radius: 10px;
-        max-width: 400px;
-        margin: 10% auto;
-        text-align: center;
-    }
-
-    .promo .modal{
-        background: crimson;
-        color: white;
-    }
-
+  .backdrop{
+    width: 100%;
+    height: 100%;
+    position: fixed;
+    background: rgba(0,0,0,0.8);
+  }
+  .modal{
+    padding: 10px;
+    border-radius: 10px;
+    max-width: 400px;
+    margin: 10% auto;;
+    text-align: center;
+    background: white;
+  }
+  .promo .modal{
+    background: crimson;
+    color: white;
+  }
 </style>
